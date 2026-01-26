d4c75c6bc385e69d34507154f7c80dddae41e6e6
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2026-01-26 13:51:10 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -23,11 +23,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/prompts')
 const allowedOrigins = [
     'https://prompts-collect.vercel.app',
     'https://nanoprompts.space',
-    'https://www.nanoprompts.space',
-
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'http://localhost:5173'
 ];
 
 app.use(cors({
